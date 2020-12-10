22451c8c22d507a7e467357ad695b924c2b2a112
Bamidele <bamideleomonayin@gmail.com>
2020-12-10 17:05:46 +0100

    adding google map view

--- app/src/main/java/com/example/farmmarket/farmDetail.java
+++ app/src/main/java/com/example/farmmarket/farmDetail.java
@@ -10,13 +10,20 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class farmDetail extends AppCompatActivity {
+public class farmDetail extends AppCompatActivity implements OnMapReadyCallback {
 
     private ImageView imageView ;
     private TextView textViewName,textViewDesc;
     private Farm farm;
     private Button butMap;
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +33,8 @@ public class farmDetail extends AppCompatActivity {
         textViewDesc = findViewById(R.id.completedesc);
         butMap = findViewById(R.id.buttonMap);
 
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        supportMapFragment.getMapAsync(this);
         farm = (Farm) getIntent().getSerializableExtra(MainActivity.FARM_PORT);
         textViewName.setText(farm.getTitle());
         textViewDesc.setText(farm.getDescription());
@@ -34,8 +43,21 @@ public class farmDetail extends AppCompatActivity {
                 .load(Uri.parse("https://cdn.pixabay.com/photo/2013/11/23/13/57/barn-216372_960_720.jpg"))
                 .into(imageView);
 
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker in hyderabad and move the camera
         butMap.setOnClickListener(v->{
-            Log.d("FarmDetail","I should go to google Map");
+            if(mMap != null){
+                LatLng hyderadbad = new LatLng(17, 78);
+                mMap.addMarker(new MarkerOptions().position(hyderadbad).title("Garri farm"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(hyderadbad,20));
+            }
+
         });
+
     }
 }
