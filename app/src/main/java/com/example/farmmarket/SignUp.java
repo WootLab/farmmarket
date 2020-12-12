3b2e004eb9cb61896dfca957accafddb5c865c1b
Bamidele <bamideleomonayin@gmail.com>
2020-12-12 07:55:38 +0100

    after d green tick by removing unused imports

--- app/src/main/java/com/example/farmmarket/SignUp.java
+++ app/src/main/java/com/example/farmmarket/SignUp.java
@@ -4,15 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
-import com.google.android.material.button.MaterialButton;
-import com.google.android.material.textfield.TextInputEditText;
 
 public class SignUp extends AppCompatActivity {
 
